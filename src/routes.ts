efe4909e1ab8fd7b3de3ec720cfe3b7134a22415
Cauã Clemente <cauaclemente07@gmail.com>
2024-04-02 20:00:34 -0300

    Fazendo login do usuário

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,14 @@
 import { Router, Request, Response } from "express";
 
 import { CreateUserController } from "./controllers/user/CreateUserController";
+import { AuthUserController } from "./controllers/user/AuthUserController";
 
 const router = Router();
 
+// --- Rotas User ---
 router.post('/users', new CreateUserController().handle)
+router.post('/session', new AuthUserController().handle)
+
+
 
 export {router};
