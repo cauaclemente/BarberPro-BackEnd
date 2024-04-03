0c203e57f7cff11d791d94069102143dd90c4155
Cauã Clemente <cauaclemente07@gmail.com>
2024-04-03 15:03:16 -0300

    Detalhes do usuário

--- src/controllers/user/DetailUserController.ts
+++ src/controllers/user/DetailUserController.ts
@@ -4,9 +4,11 @@ import { DetailUserService } from "../../services/user/DetailUserService";
 class DetailUserController{
   async handle(req: Request, res: Response){
 
+    const user_id = req.user_id;
+
     const detaileUserService = new DetailUserService();
 
-    const datailUser = await detaileUserService.execute();
+    const datailUser = await detaileUserService.execute(user_id);
   
     return res.json(datailUser)
   }
--- src/services/user/DetailUserService.ts
+++ src/services/user/DetailUserService.ts
@@ -1,9 +1,28 @@
 import prismaClient from "../../prisma";
 
 class DetailUserService{
-  async execute(){
+  async execute( user_id: string){
 
-    return{ok: true}
+    const user = await prismaClient.user.findFirst({
+      where:{
+        id: user_id
+      },
+      select:{
+        id: true,
+        name: true,
+        email: true,
+        endereco: true,
+        subscriptions:{
+          select:{
+            id: true,
+            priceId: true,
+            status: true,
+          }
+        }
+      }
+    })
+
+    return user;
   }
 }
 
